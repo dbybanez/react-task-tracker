c9b779cdae35d98a830640ff7e1b763849921d37
davidybanez <david.ybanez@techmesh.co.uk>
2022-09-29 22:26:33 +0800

    initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+// import React from 'react'
+import Header from './components/Header'
+
+function App() {
+
+  return (
+    <div className="container">
+      <Header title='Task Tracker'/>
+    </div>
+  );
+}
+
+// class App extends React.Component {
+//   render() {
+//     return (
+//       <Header/>
+//     )
+//   }
+// }
+
+export default App;
