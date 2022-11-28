16aa8984fa8b7d9148278f647cd008f3530b84c7
davidybanez <david.ybanez@techmesh.co.uk>
2022-11-28 16:15:04 +0800

    add toggleReminder method

--- src/App.js
+++ src/App.js
@@ -25,10 +25,24 @@ function App() {
     }
   ])
 
+  // Delete Task
+  const deleteTask = (id) => {
+    // console.log('delete', id)
+    setTasks(tasks.filter((task) => task.id !== id))
+  }
+
+  // Toggle Reminder
+  const toggleReminder = (id) => {
+    // console.log(id)
+    setTasks(tasks.map((task) => task.id === id? {} : task))
+  }
+
   return (
     <div className="container">
       <Header title='Task Tracker'/>
-      <Tasks tasks={tasks} />
+      {tasks.length > 0 ? 
+        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/> 
+        : 'No tasks to show.'}
     </div>
   );
 }
