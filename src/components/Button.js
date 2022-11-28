0b3e8b77c9a1af778fb0c2b03891f404063111e0
davidybanez <david.ybanez@techmesh.co.uk>
2022-11-28 14:31:19 +0800

    move onClick function to Header component

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,9 +1,6 @@
 import PropTypes from 'prop-types'
 
-const Button = ({ color, text }) => {
-  const onClick = (e) => {
-    console.log(e)
-  }
+const Button = ({ color, text, onClick }) => {
 
   return (
     <button
@@ -22,7 +19,8 @@ Button.defaultProps = {
 
 Button.propTypes = {
   text: PropTypes.string,
-  color: PropTypes.string
+  color: PropTypes.string,
+  onClick: PropTypes.func
 }
 
 export default Button
