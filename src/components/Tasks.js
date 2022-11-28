1beed246cd79ac89d01378e374173d9483e51c18
davidybanez <david.ybanez@techmesh.co.uk>
2022-11-28 15:07:45 +0800

    add Task component, refactor Tasks component

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,31 +1,11 @@
 import { Fragment } from "react"
+import Task from './Task'
 
-const tasks = [
-  {
-    id: 1,
-    text: 'Doctors Appointment',
-    day: 'Feb 5th at 2:30pm',
-    reminder: true
-  },
-  {
-    id: 2,
-    text: 'Meeting at School',
-    day: 'Feb 6th at 1:30pm',
-    reminder: true
-  },
-  {
-    id: 3,
-    text: 'Food Shopping',
-    day: 'Feb 5th at 2:30pm',
-    reminder: false
-  }
-]
-
-const Tasks = () => {
+const Tasks = ({ tasks }) => {
   return (
     <Fragment>
       {tasks.map((task) => (
-        <h3 key={task.id}>{task.text}</h3>
+        <Task key={task.id} task={task} />
       ))}
     </Fragment>
   )
