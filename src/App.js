cab428f047f7a8c5462b298ca26de6adea79e9f3
davidybanez <david.ybanez@techmesh.co.uk>
2022-11-28 18:07:12 +0800

    add server requests functionality

--- src/App.js
+++ src/App.js
@@ -42,27 +42,64 @@ function App() {
 
     return data
   }
+
+   // Fetch task
+   const fetchTask = async (id) => {
+    const res = await fetch(`http://localhost:5000/tasks/${id}`)
+    const data = await res.json()
+
+    return data
+  }
    
   // Add Task
-  const addTask = (task) => {
+  const addTask = async (task) => {
+    const res = await fetch('http://localhost:5000/tasks',
+      {
+        method: 'POST',
+        headers: {
+          'Content-type': 'application/json'
+        },
+        body: JSON.stringify(task)
+      })
+
+    const data = await res.json()
+
+    setTasks([...tasks, data])
+
     // console.log(task)
-    const id = Math.floor(Math.random() * 10000) + 1
-    const newTask = {id, ...task }
-    setTasks([...tasks, newTask])
+    // const id = Math.floor(Math.random() * 10000) + 1
+    // const newTask = {id, ...task }
+    // setTasks([...tasks, newTask])
   }
 
   // Delete Task
-  const deleteTask = (id) => {
+  const deleteTask = async (id) => {
     // console.log('delete', id)
+    await fetch(`http://localhost:5000/tasks/${id}`, {method: 'DELETE'})
+
     setTasks(tasks.filter((task) => task.id !== id))
   }
 
   // Toggle Reminder
-  const toggleReminder = (id) => {
+  const toggleReminder = async (id) => {
+    const taskToToggle = await fetchTask(id)
+    const updTask = {...taskToToggle, reminder: !taskToToggle.reminder}
+
+    const res = await fetch(`http://localhost:5000/tasks/${id}`, 
+      {
+        method: 'PUT',
+        headers: {
+          'Content-type': 'application/json'
+        },
+        body: JSON.stringify(updTask)
+      })
+
+    const data = await res.json()
+
     // console.log(id)
     setTasks(
       tasks.map((task) => 
-        task.id === id? {...task, reminder: !task.reminder} : task
+        task.id === id? {...task, reminder: data.reminder} : task
       )
     )
   }
@@ -75,9 +112,11 @@ function App() {
         showAdd={showAddTask}
       />
       {showAddTask && <AddTask onAdd={addTask} />}
-      {tasks.length > 0 ? 
-        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/> 
-        : 'No tasks to show.'}
+      {tasks.length > 0 ? (
+        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/>
+      ) : (
+        'No tasks to show.'
+      )}
     </div>
   );
 }
