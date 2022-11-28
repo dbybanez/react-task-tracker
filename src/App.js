2e8ea0e9d3428331cb53dff45a90cc54fb931551
davidybanez <david.ybanez@techmesh.co.uk>
2022-11-28 16:20:54 +0800

    add class on double click

--- src/App.js
+++ src/App.js
@@ -34,7 +34,11 @@ function App() {
   // Toggle Reminder
   const toggleReminder = (id) => {
     // console.log(id)
-    setTasks(tasks.map((task) => task.id === id? {} : task))
+    setTasks(
+      tasks.map((task) => 
+        task.id === id? {...task, reminder: !task.reminder} : task
+      )
+    )
   }
 
   return (
