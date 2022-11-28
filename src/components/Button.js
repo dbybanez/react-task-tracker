0fb204e635d20a06d2ab37a6cf751fc720d90250
davidybanez <david.ybanez@techmesh.co.uk>
2022-11-28 14:26:22 +0800

    add click function

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,9 +1,16 @@
 import PropTypes from 'prop-types'
 
 const Button = ({ color, text }) => {
+  const onClick = (e) => {
+    console.log(e)
+  }
+
   return (
-    <button style={{ backgroundColor: color}} 
-    className='btn'>
+    <button
+      onClick={onClick}
+      style={{ backgroundColor: color}} 
+      className='btn'
+    >
       {text}
     </button>
   )
